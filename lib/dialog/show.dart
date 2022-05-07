6af3dcbb468d87a6089398dcfa86d549013e3d2e
DANCHE <dangdancheg@gmail.com>
2022-05-07 22:32:25 +0300

    fixes in dialogs

--- lib/dialog/show.dart
+++ lib/dialog/show.dart
@@ -1,7 +1,7 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:flutter/material.dart';
 
 const dialogWidth = 380.0;
+const showCloseIcon = true;
 
 enum NotificationType {
   protoSaved,
@@ -14,36 +14,33 @@ void showNotification(context, NotificationType type) {
     AwesomeDialog(
       context: context,
       dialogType: DialogType.SUCCES,
-      animType: AnimType.SCALE,
       headerAnimationLoop: false,
       title: 'Saved!',
       desc: 'You can explore that proto API definition in protos tab.',
       width: dialogWidth,
-      showCloseIcon: true,
+      showCloseIcon: showCloseIcon,
     ).show();
   }
   if (type == NotificationType.protoSaveError) {
     AwesomeDialog(
       context: context,
       dialogType: DialogType.ERROR,
-      animType: AnimType.SCALE,
       headerAnimationLoop: false,
       title: 'Error!',
       desc: 'Unable to save proto, unknown error.',
       width: dialogWidth,
-      showCloseIcon: true,
+      showCloseIcon: showCloseIcon,
     ).show();
   }
   if (type == NotificationType.protoParseError) {
     AwesomeDialog(
       context: context,
       dialogType: DialogType.ERROR,
-      animType: AnimType.SCALE,
       headerAnimationLoop: false,
       title: 'Error!',
       desc: 'Unable to parse proto file, check proto file.',
       width: dialogWidth,
-      showCloseIcon: true,
+      showCloseIcon: showCloseIcon,
     ).show();
   }
 }
