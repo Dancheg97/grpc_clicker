774bc4e90c9927c09f2a65b3b9245daa172debcf
DANCHE <dangdancheg@gmail.com>
2022-05-08 22:04:03 +0300

    added panels for requst and response

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # grpc_rocket
 
 cd ..;cd gRPC_api_example;go run .
+grpcurl -proto api.proto -d '{"message": 12.123}' -plaintext localhost:12201 pb.Basics/DoubleCall
+grpcurl -import-path . -proto C:\Users\dangd\OneDrive\Документы\gRPC_api_example\api.proto describe
 
 ## TODO
 
 - add check if grpcurl installed
+- Send grpcurl message to service ``
--- lib/request/frame.dart
+++ lib/request/frame.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:grpc_rocket/navigator/frame.dart';
+import 'package:grpc_rocket/request/request.dart';
+import 'package:grpc_rocket/request/response.dart';
+import 'package:multi_split_view/multi_split_view.dart';
 import 'package:grpc_rocket/request/adress.dart';
 
 class RequstFrame extends StatelessWidget {
@@ -16,8 +20,27 @@ class RequstFrame extends StatelessWidget {
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.send_rounded),
-            )
+            ),
           ],
+        ),
+        MultiSplitViewTheme(
+          data: MultiSplitViewThemeData(
+            dividerPainter: DividerPainters.background(
+              color: Colors.blueGrey[200],
+              highlightedColor: Colors.blueGrey[500],
+            ),
+            dividerThickness: 3,
+          ),
+          child: MultiSplitView(
+            initialAreas: [
+              Area(weight: 0.5, minimalSize: 150),
+              Area(minimalSize: 150),
+            ],
+            children: const [
+              RequestPane(),
+              ResponsePane(),
+            ],
+          ),
         ),
       ],
     );
