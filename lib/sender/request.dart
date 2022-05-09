436119eda85fb02b84706c4cbdfea0d504ed4407
DANCHE <dangdancheg@gmail.com>
2022-05-09 22:48:04 +0300

    replaced json bullshit editor with custom textfield

--- lib/sender/request.dart
+++ lib/sender/request.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:grpc_rocket/providers/request.dart';
-import 'package:json_editor/json_editor.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -23,61 +22,61 @@ class RequestPane extends StatelessWidget {
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blueGrey[500]!,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(12)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer<RequestNotifier>(
-                  builder: (context, model, child) {
-                    return JsonEditor.string(
-                      jsonString: model.request,
-                      onValueChanged: (value) async {
-                        var prefs = await SharedPreferences.getInstance();
-                        prefs.setString('req', value.toString());
-                      },
-                    );
-                  },
-                ),
-              ),
-            ),
-          ),
-        ),
-        Center(
-          child: Text(
-            'grpcurl description',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blueGrey[500]!,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(12)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer<RequestNotifier>(
-                  builder: (context, model, child) {
-                    return JsonEditor.string(
-                      jsonString: model.description,
-                      onValueChanged: (value) {},
-                    );
-                  },
-                ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Consumer<RequestNotifier>(
+                builder: (context, model, child) {
+                  return RequestTextField(
+                    key: UniqueKey(),
+                    startText: model.request,
+                  );
+                },
               ),
             ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class RequestTextField extends StatefulWidget {
+  final String startText;
+  const RequestTextField({
+    Key? key,
+    required this.startText,
+  }) : super(key: key);
+
+  @override
+  State<RequestTextField> createState() => _RequestTextFieldState();
+}
+
+class _RequestTextFieldState extends State<RequestTextField> {
+  var controller = TextEditingController();
+
+  @override
+  void initState() {
+    controller.text = widget.startText;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      decoration: InputDecoration(
+        hintText: 'grpc requst input field',
+        contentPadding: const EdgeInsets.all(15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      onChanged: (text) async {
+        var prefs = await SharedPreferences.getInstance();
+        prefs.setString('adress', text);
+      },
     );
   }
 }
