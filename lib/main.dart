9fc71168e758babafe1036812f29e3262a53de50
DANCHE <dangdancheg@gmail.com>
2022-05-10 21:22:15 +0300

    added windows window customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:grpc_rocket/navigator/frame.dart';
-import 'package:grpc_rocket/providers/request.dart';
-import 'package:grpc_rocket/providers/response.dart';
-import 'package:grpc_rocket/sender/frame.dart';
-import 'package:multi_split_view/multi_split_view.dart';
-import 'package:provider/provider.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => RequestNotifier()),
-        ChangeNotifierProvider(create: (context) => ResponseNotifier()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    final initialSize = Size(600, 450);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.center;
+    win.title = "Custom window with Flutter";
+    win.show();
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+const borderColor = Color(0xFF805306);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        dividerTheme: DividerThemeData(
-          color: Colors.blueGrey[200],
-          thickness: 3,
-        ),
-      ),
-      home: Scaffold(
-        body: MultiSplitViewTheme(
-          data: MultiSplitViewThemeData(
-            dividerPainter: DividerPainters.background(
-              color: Colors.blueGrey[200],
-              highlightedColor: Colors.blueGrey[500],
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: WindowBorder(
+                color: borderColor,
+                width: 1,
+                child: Row(children: [LeftSide(), RightSide()]))));
+  }
+}
+
+const sidebarColor = Color(0xFFF6A00C);
+
+class LeftSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 200,
+        child: Container(
+            color: sidebarColor,
+            child: Column(
+              children: [
+                WindowTitleBarBox(child: MoveWindow()),
+                Expanded(child: Container())
+              ],
+            )));
+  }
+}
+
+const backgroundStartColor = Color(0xFFFFD500);
+const backgroundEndColor = Color(0xFFF6A00C);
+
+class RightSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [backgroundStartColor, backgroundEndColor],
+                  stops: [0.0, 1.0]),
             ),
-            dividerThickness: 3,
-          ),
-          child: MultiSplitView(
-            initialAreas: [
-              Area(weight: 0.26, minimalSize: 272),
-              Area(minimalSize: 520),
-            ],
-            children: const [
-              NavigatorFrame(),
-              RequstFrame(),
-            ],
-          ),
-        ),
-      ),
+            child: Column(children: [
+              WindowTitleBarBox(
+                  child: Row(children: [
+                Expanded(child: MoveWindow()),
+                WindowButtons()
+              ])),
+            ])));
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: Color(0xFF805306),
+    mouseOver: Color(0xFFF6A00C),
+    mouseDown: Color(0xFF805306),
+    iconMouseOver: Color(0xFF805306),
+    iconMouseDown: Color(0xFFFFD500));
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: Color(0xFFD32F2F),
+    mouseDown: Color(0xFFB71C1C),
+    iconNormal: Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
     );
   }
 }
