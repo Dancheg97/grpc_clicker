3ef2930ea6be1d9549587579ece6218e62e2a027
Danila Fomin <dangdancheg@gmail.com>
2022-05-12 20:32:10 +0300

    added service structure

--- lib/left/service.dart
+++ lib/left/service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grpc_rocket/colors.dart';
 import 'package:grpc_rocket/grpcurl.dart';
 import 'package:grpc_rocket/left/method.dart';
 
@@ -11,15 +12,57 @@ class ServiceTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const VerticalDivider(),
-        Column(
-          children: service.methods.map((e) {
-            return MethodTab(method: e);
-          }).toList(),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+      child: IntrinsicHeight(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Divider(
+              color: Palette.white,
+              indent: 16,
+              endIndent: 16,
+            ),
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Icon(
+                      Icons.account_tree_rounded,
+                      color: Palette.white,
+                      size: 16,
+                    ),
+                  ),
+                  Text(
+                    service.showName,
+                    style: TextStyle(
+                      color: Palette.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            IntrinsicHeight(
+              child: Row(
+                children: [
+                  VerticalDivider(color: Palette.white),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: service.methods.map((e) {
+                        return MethodTab(method: e);
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
