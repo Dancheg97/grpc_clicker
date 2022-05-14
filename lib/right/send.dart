10b968e81d3ea196bf92e3f0986fc4f9d08c0799
Danila Fomin <dangdancheg@gmail.com>
2022-05-14 17:50:43 +0300

    added button for sending requests

--- lib/right/send.dart
+++ lib/right/send.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:grpc_clicker/colors.dart';
 
 class SendButton extends StatelessWidget {
   const SendButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Palette.black,
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+          child: TextButton(
+            onPressed: () {},
+            child: SizedBox(
+              width: 32,
+              height: double.infinity,
+              child: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Palette.white,
+                size: 32,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
